f4d0e728357aae1f7577f660f813dee4e117a8b3
Victor <victor.alves.sales@hotmail.com>
2023-03-28 10:58:03 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import React from 'react';
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 
+function teste(){
+
+
+  return(
+    <h1>oi</h1>
+  )
+}
+
 
 function App() {
   const [posts, setPosts] = useState([]);
